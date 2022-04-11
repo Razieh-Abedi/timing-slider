6a33ff6974d57e8557ff3e0d69503ebe4cd49bdc
razinewworld <raziyeabedi@gmail.com>
2022-04-11 22:55:52 +0800

    wrote the function for the left arrow of the slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,34 @@
 ////////*****change slides by clicking on arrows*******//////
 let sliderImgs = document.querySelectorAll(".slider-img");
+let index = 0;
+sliderImgs[index].style.display = "block";
+// changing slides by clicking on the right arrow/////
 document.getElementById("next-btn").addEventListener("click", () => {
-  let sliderImgTemp = sliderImgs[0];
-  let sliderToShow;
-  for (let index = 0; index < sliderImgs.length; index++) {
-    if (sliderImgs[index] == sliderImgTemp) {
-      sliderImgs[index].style.display = "none";
-      sliderImgs[index].style.display = "block";
-      sliderImgTemp++;
-    }
-    // sliderImgs[index].style.display = "none";
-    // sliderImgs[0].style.display = "block";
-    // sliderImgTemp += sliderImgs[index].style.display = "block";
+  /////*******loop through the slides////
+  sliderImgs[index].style.display = "none"; //current
+  index++;
+  if (index >= sliderImgs.length) {
+    index = 0;
   }
+  sliderImgs[index].style.display = "block"; //next
+  //*******/ slides stop at the last image////
+  // if (index < sliderImgs.length-1) {
+  //   sliderImgs[index].style.display = "none";
+  //   index++;
+  //   sliderImgs[index].style.display = "block";
+  // }
 });
-
-document.getElementById("previous-btn").addEventListener("click", () => {
-  for (let index = 0; index < sliderImgs.length; index++) {
-    sliderImgs[index].style.display = "none";
-    sliderImgs[0].style.display = "block";
-  }
-});
+// changing slides by clicking on the left arrow/////
+// document.getElementById("previous-btn").addEventListener("click", () => {
+//   sliderImgs[index].style.display = "none";
+//   index = sliderImgs.length;
+//   sliderImgs[index].style.display = "block";
+//   index--;
+//   if ((index = sliderImgs[0])) {
+//     index = sliderImgs.length + 1;
+//   }
+//   sliderImgs[index].style.display = "block";
+// });
 
 ////********change slides and the opacity of indicators by clicking on indicators******//////
 // document
@@ -31,6 +39,7 @@ document.getElementById("previous-btn").addEventListener("click", () => {
 //       sliderImgs[0].style.display = "block";
 //     }
 //   });
+
 document.getElementById("indicator-btn1").addEventListener("click", () => {
   let indicatorBtns = document.getElementsByClassName("indicator-btn");
   for (let index = 0; index < sliderImgs.length; index++) {
@@ -42,3 +51,8 @@ document.getElementById("indicator-btn1").addEventListener("click", () => {
     indicatorBtns[0].style.opacity = "1";
   }
 });
+
+// let indicatorBtns = document.getElementsByClassName("indicator-btn");
+// indicatorBtns.forEach(element => {
+//     element.addEventListener
+// });
