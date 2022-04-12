a6b33f28dbba02a8e8cb7c86d41371e0b03e5b08
razinewworld <raziyeabedi@gmail.com>
2022-04-12 16:20:26 +0800

    set timer for slides, wrote functions for indicators

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,58 +1,57 @@
-////////*****change slides by clicking on arrows*******//////
 let sliderImgs = document.querySelectorAll(".slider-img");
 let index = 0;
 sliderImgs[index].style.display = "block";
-// changing slides by clicking on the right arrow/////
-document.getElementById("next-btn").addEventListener("click", () => {
-  /////*******loop through the slides////
+function nextSlide() {
   sliderImgs[index].style.display = "none"; //current
   index++;
   if (index >= sliderImgs.length) {
     index = 0;
   }
   sliderImgs[index].style.display = "block"; //next
-  //*******/ slides stop at the last image////
-  // if (index < sliderImgs.length-1) {
-  //   sliderImgs[index].style.display = "none";
-  //   index++;
-  //   sliderImgs[index].style.display = "block";
-  // }
+}
+
+let indicatorBtns = document.querySelectorAll(".indicator-btn");
+let i = 0;
+indicatorBtns[i].style.opacity = "1";
+function nextIndicator() {
+  indicatorBtns[i].style.opacity = "0.5";
+  i++;
+  if (i >= indicatorBtns.length) {
+    i = 0;
+  }
+  indicatorBtns[i].style.opacity = "1";
+}
+// changing slides by clicking on the right arrow/////
+document.getElementById("next-btn").addEventListener("click", () => {
+  nextSlide();
+  nextIndicator();
 });
-// changing slides by clicking on the left arrow/////
+////********change slides and indicators by clicking on indicators******//////
+for (let index = 0; index < indicatorBtns.length; index++) {
+  indicatorBtns[index].addEventListener("click", () => {
+    nextIndicator();
+    nextSlide();
+  });
+}
+////*********Timer sliders */
+setInterval(() => {
+  nextSlide();
+  nextIndicator();
+}, 10000);
+
+/////changing slides by clicking on the left arrow/////
+// let sliderImgs = document.querySelectorAll(".slider-img");
+// let index2 = sliderImgs.length - 1;
+// sliderImgs[0].style.display = "block";
+
 // document.getElementById("previous-btn").addEventListener("click", () => {
-//   sliderImgs[index].style.display = "none";
-//   index = sliderImgs.length;
-//   sliderImgs[index].style.display = "block";
-//   index--;
-//   if ((index = sliderImgs[0])) {
-//     index = sliderImgs.length + 1;
+//   for (let index = 0; index < sliderImgs.length; index++) {
+//     sliderImgs[index].style.display = "none";
 //   }
-//   sliderImgs[index].style.display = "block";
-// });
-
-////********change slides and the opacity of indicators by clicking on indicators******//////
-// document
-//   .getElementsByClassName("indicator-btn")
-//   .addEventListener("click", () => {
-//     for (let index = 0; index < sliderImgs.length; index++) {
-//       sliderImgs[index].style.display = "none";
-//       sliderImgs[0].style.display = "block";
-//     }
-//   });
-
-document.getElementById("indicator-btn1").addEventListener("click", () => {
-  let indicatorBtns = document.getElementsByClassName("indicator-btn");
-  for (let index = 0; index < sliderImgs.length; index++) {
-    sliderImgs[index].style.display = "none";
-    sliderImgs[0].style.display = "block";
-  }
-  for (let index = 0; index < indicatorBtns.length; index++) {
-    indicatorBtns[index].style.opacity = "0.5";
-    indicatorBtns[0].style.opacity = "1";
-  }
-});
-
-// let indicatorBtns = document.getElementsByClassName("indicator-btn");
-// indicatorBtns.forEach(element => {
-//     element.addEventListener
+//   sliderImgs[index2].style.display = "block";
+//   index2--;
+//   if ((index2 = sliderImgs[0])) {
+//     index2 = sliderImgs.length - 1;
+//   }
+//   sliderImgs[index2].style.display = "block";
 // });
